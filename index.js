b7d7dde71659499190e0cef84cdfd7355009fe81
Timothy Montgomery <tim.montgomery95@gmail.com>
2022-08-08 13:54:15 -0400

    commit tree e3ec7daf43a099b71d6cd29719d4a6e070f5e279

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let num3 = 100
+let num4 = 24
+
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4
+const max = Math.max(1, 2, 3, 4, 20);
